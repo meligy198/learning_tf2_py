376f5c50c48c677c0ef00c61e28816f1ae1fe95b
meligy198 <ahmedomar98@gmail.com>
2022-10-13 11:50:36 +0400

    I changed the namespace just as a test

--- launch/turtle_tf2_demo.launch.py
+++ launch/turtle_tf2_demo.launch.py
@@ -10,7 +10,7 @@ def generate_launch_description():
         Node(
             package='turtlesim',
             executable='turtlesim_node',
-            name='sim'
+            name='simulation'
         ),
         Node(
             package='learning_tf2_py',
